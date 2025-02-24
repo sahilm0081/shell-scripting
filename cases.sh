1fb7f75799cdd0111a2586bf13b307705ba1bc3e
Sahil Madan <sahil.madan@mymedisage.com>
2025-02-24 15:43:37 +0530

    commit file

---
+++ cases.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+x=7
+
+case $x in
+    5) echo "hi";;
+    7) echo "hello";;
+    *)echo "provide valid input"
+esac   
---
+++ conditions.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+x=7
+
+if [ "$x" -lt 5 ]; then
+    echo "x less than 5"
+else
+    echo "x greater than 5"
+fi    
